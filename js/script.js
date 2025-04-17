20b39b53600ca1acf4441880d54c6d884ebb9fc2
Hernandez <priambodohernandez@gmail.com>
2025-04-17 10:14:34 +0700

    done

--- js/script.js
+++ js/script.js
@@ -24,12 +24,12 @@ function handleFormSubmit(e) {
     e.preventDefault();
 
     const name = document.getElementById("nama").value;
-    const dob = document.getElementById("date").value;
+    const tanggal = document.getElementById("date").value;
     const gender = document.querySelector('input[name="gender"]:checked')?.value;
     const message = document.getElementById("message").value;
 
     document.getElementById("result-nama").innerText = name || "-";
-    document.getElementById("result-date").innerText = dob || "-";
+    document.getElementById("result-date").innerText = tanggal || "-";
     document.getElementById("result-gender").innerText = gender || "-";
     document.getElementById("result-message").innerText = message || "-";
     
